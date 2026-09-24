5dfa98e72ab9939b24904fca06979bd2e0723033
recompcore <adambouragba3@gmail.com>
2026-09-24 22:14:01 +0200

    Delete ports/LegacyRecomp directory

--- ports/LegacyRecomp/src/main.cpp
+++
@@ -1,7 +0,0 @@
-// legacyrecomp - ReXGlue Recompiled Project
-
-#include "generated/default/legacyrecomp_init.h"
-
-#include "legacyrecomp_app.h"
-
-REX_DEFINE_APP(legacyrecomp, LegacyrecompApp::Create)
