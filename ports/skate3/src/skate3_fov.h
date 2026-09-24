e9b83237f292e6963328db9b7610d88aa9876b6d
recompcore <adambouragba3@gmail.com>
2026-09-24 22:07:38 +0200

    Delete ports/skate3 directory

--- ports/skate3/src/skate3_fov.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void Skate3InitializeFieldOfViewOverride();
-void Skate3UpdateFieldOfViewOverride(double degrees);
-float Skate3MaybeOverrideProjectionFovRadians(float native_radians);
